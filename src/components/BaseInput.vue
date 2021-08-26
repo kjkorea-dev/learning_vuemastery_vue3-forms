0318b085ce0d9e5388bb42d6c30e050772f84e29
kj <agassi77@gmail.com>
2021-08-26 18:42:09 +0900

    Basic a11y : accesible errors

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -7,7 +7,16 @@
     @input="$emit('update:modelValue', $event.target.value)"
     class="field"
     :id="uuid"
+    :aria-describedby="error ? `${uuid}-error` : null"
   />
+  <p
+    v-if="error"
+    class="errorMessage"
+    :id="`${uuid}-error`"
+    aria-live="assertive"
+  >
+    {{ error }}
+  </p>
 </template>
 <script>
 import UniqueID from '@/features/UniqueID.js'
@@ -19,6 +28,10 @@ export default {
     },
     modelValue: {
       type: [String, Number],
+      default: '',
+    },
+    error: {
+      type: String,
       default: '',
     },
   },
